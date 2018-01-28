77479ed7b3005bff9c24a75ebb6a708ec388e557
gracecuenca <gcuenca@stuy.edu>
2018-01-28 06:53:35 -0500

    graphs

--- extension/js/line.js
+++ extension/js/line.js
@@ -9,8 +9,8 @@ var start = "2018-01-20";
 var end = "2018-01-26";
 var dates = ["01/20", "01/21", "01/22", "01/23", "01/24", "01/25", "01/26"]
 // For drawing the lines
-var liberal = [6,1,3,2,9,8,7];
-var conservative = [0,3,2,2,1,3,2];
+var liberal = [5,7,9,8,7,5,6];
+var conservative = [0,1,0,0,2,3,5];
 
 var lineChart = new Chart(document.getElementById("lineChart"), {
   type: 'line',
