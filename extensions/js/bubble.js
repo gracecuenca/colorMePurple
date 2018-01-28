ddf833c864b88167a4d5e019b10ba02264addf15
gracecuenca <gcuenca@stuy.edu>
2018-01-27 21:03:16 -0500

    beautified

--- extensions/js/bubble.js
+++ extensions/js/bubble.js
@@ -6,6 +6,15 @@
 //label of dots will be news sources
 //hardcoded for now
 //will retrieve from data base
+var ctx = document.getElementById("bubbleChart").getContext("2d");
+
+/*** Gradient ***/
+//blue: 18, 13, 226
+//red: 235, 29, 44
+var gradient = ctx.createLinearGradient(0,0,1600,0);
+gradient.addColorStop(0, 'rgba(18,13,226,1)');
+gradient.addColorStop(1, 'rgba(235,29,44,0)');
+/******* *********/
 
 var bubbleChart = new Chart(document.getElementById("bubbleChart"), {
   type: 'bubble',
@@ -13,7 +22,7 @@ var bubbleChart = new Chart(document.getElementById("bubbleChart"), {
     datasets: [
       {
         label: ['New York Times'],
-        backgroundColor: "rgba(255,221,50,0.2)",
+        backgroundColor:"rgba(255,221,50,1)",
         borderColor: "rgba(255,221,50,1)",
         data: [{
           x: -2500,
@@ -22,7 +31,7 @@ var bubbleChart = new Chart(document.getElementById("bubbleChart"), {
         }]
       }, {
         label: ['The New Yorker'],
-        backgroundColor: "rgba(60,186,159,0.2)",
+        backgroundColor: "rgba(60,186,159,1)",
         borderColor: "rgba(60,186,159,1)",
         data: [{
           x: -2000,
@@ -31,7 +40,7 @@ var bubbleChart = new Chart(document.getElementById("bubbleChart"), {
         }]
       }, {
         label: ['Wall Street Journal'],
-        backgroundColor: "rgba(193,46,12,0.2)",
+        backgroundColor: "rgba(193,46,12,1)",
         borderColor: "rgba(193,46,12,1)",
         data: [{
           x: 2000,
@@ -42,20 +51,32 @@ var bubbleChart = new Chart(document.getElementById("bubbleChart"), {
     ]
   },
   options: {
-      title: {
-        display: true,
-        text: 'Gradient of News Sources Viewed and Frequency'
-      }, scales: {
+    legend: {
+      labels: {
+        fontColor: "black"
+      }
+    },
+    scales: {
         yAxes: [{
+          ticks: {
+            display: false,
+            fontColor: "black"
+          },
           scaleLabel: {
             display: true,
-            labelString: "Accuracy"
+            labelString: "Accuracy",
+            fontColor: "black"
           }
         }],
         xAxes : [{
+          ticks: {
+            display: false,
+            fontColor: "black"
+          },
           scaleLabel: {
             display: true,
-            labelString: "Political Lean"
+            labelString: "Political Lean",
+            fontColor: "black"
           }
         }]
       }
